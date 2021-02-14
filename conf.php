604ed9472d18b20f8170f8b3083f19847530fd05
Fizruk02 <79018173+Fizruk02@users.noreply.github.com>
2021-02-14 17:25:35 +0300

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?
+$config = [
+	'mail' => 'test@mail.ru',
+	'pass' => 'pass',
+	'domain' => '{imap.mail.ru:143}INBOX'
+];
+
+$mySql = [
+	'ip' => '127.0.0.1',
+	'log' => 'log',
+	'pass' => 'pass',
+	'bData' => 'bData'
+];
